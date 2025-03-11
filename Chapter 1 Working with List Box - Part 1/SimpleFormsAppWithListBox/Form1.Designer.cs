d2d836703f7e8d93f4f1007e81a007073a0c7324
BryceRussinBuendia <150444483+BryceRussinBuendia@users.noreply.github.com>
2025-03-11 22:20:49 +0800

    Add files via upload

---
+++ Chapter 1 Working with List Box - Part 1/SimpleFormsAppWithListBox/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SimpleFormsAppWithListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(156, 74);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(225, 74);
+            button2.Name = "button2";
+            button2.Size = new Size(53, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(284, 74);
+            button3.Name = "button3";
+            button3.Size = new Size(52, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(30, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(316, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Porgramming Language Names";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.ButtonFace;
+            listBox1.ForeColor = SystemColors.WindowText;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(32, 103);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(304, 154);
+            listBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(32, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(118, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(358, 331);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+    }
+}
